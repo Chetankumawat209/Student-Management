ecb7d4e2f498b7807416fdd20df631d0b1fca23c
chetankumawat209 <chetankumawat209@gmail.com>
2026-06-07 22:33:28 +0530

    create project structure

---
+++ main.py
@@ -0,0 +1,50 @@
+import student
+
+
+def main ():
+    while True:
+        print("""
+1. Add Student
+2. View Students
+3. Search Student
+4. Update Students
+5. Delete Student
+6. Sort Students
+7. Topper Student
+8. Statistics Students
+9. Exit Student
+                            
+""")
+        obj=student.StudentManager
+        choice=input("Enter your choice")
+
+        match choice:
+            case "1":
+                obj.addStudent()
+            case "2":
+                obj.viewStudent()
+            case "3":
+                obj.seachStudent()
+            case "4":
+                obj.updateStudent()
+            case "5":
+                obj.deleteStudent()
+            case "6":
+                obj.sortStudent()
+            case "7":
+                obj.topperStudent()
+            case "8":
+                obj.studentStatistics()
+            case "9":
+                break
+            case _:
+                print("Wrong choice")
+                break
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
---
+++ student.py
@@ -0,0 +1,52 @@
+import file_manager
+
+class students:
+    def __init__(self,StudentID,Name,Age,Gender,Course,Email,Phone_Number,Marks):
+        self.StudentID=StudentID
+        self.Name=Name
+        self.Age=Age
+        self.Gender=Gender
+        self.Course=Course
+        self.Email=Email
+        self.Phone_Number=Phone_Number
+        self.Marks=Marks
+
+    def display(self):
+        print("""
+Student ID {StudentID}
+Name {Name}
+Age {Age}
+Gender {Gender}
+Course {Course}
+Email {Email}
+Phone {Phone_Number}
+Marks {Marks}
+
+              """)
+
+
+class StudentManager:
+    def __init__(self):
+       file=file_manager()
+
+    def addStudent():
+        print("add successfully")
+
+    def viewStudent():
+        pass
+
+    def seachStudent():
+        pass
+
+    def updateStudent():
+        pass
+    def deleteStudent():
+        pass
+    def sortStudent():
+        pass
+    def topperStudent():
+        pass
+
+    def studentStatistics():
+        pass
+    
